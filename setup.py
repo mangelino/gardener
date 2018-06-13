8d0700bbdc0a6e79ed290247272621ecb7cfe873
Massimiliano Angelino <massi.ang@gmail.com>
2018-06-13 20:25:41 +0200

    Missing file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gardener',
+      version='0.1',
+      description='A tool to tend greengrass',
+      url='http://github.com/massi.ang/gardener',
+      author='Massimiliano Angelino',
+      author_email='massi.ang@gmail.com',
+      license='MIT',
+      packages=['gardener'],
+      zip_safe=False)
